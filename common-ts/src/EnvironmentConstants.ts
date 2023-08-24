23680863ee2a8853563adf5e52d59df31d7ea0fc
Luke Steyn <lukecaan@gmail.com>
2023-08-24 10:52:21 +0200

    Removed unreliable RPC options

--- common-ts/src/EnvironmentConstants.ts
+++ common-ts/src/EnvironmentConstants.ts
@@ -13,21 +13,21 @@ export const EnvironmentConstants = {
 					'https://rpc-devnet.helius.xyz/?api-key=ff40c844-e15b-49d0-9d62-8534705aa48b',
 				allowAdditionalConnection: true,
 			},
-			{
-				label: 'Solana',
-				value: 'https://api.devnet.solana.com',
-				allowAdditionalConnection: false,
-			},
+			// {
+			// 	label: 'Solana',
+			// 	value: 'https://api.devnet.solana.com',
+			// 	allowAdditionalConnection: false,
+			// },
 			{
 				label: 'RPC Pool',
 				value: 'https://drift-drift-a827.devnet.rpcpool.com',
 				allowAdditionalConnection: false,
 			},
-			{
-				label: 'Ankr',
-				value: 'https://rpc.ankr.com/solana_devnet',
-				allowAdditionalConnection: false,
-			},
+			// {
+			// 	label: 'Ankr',
+			// 	value: 'https://rpc.ankr.com/solana_devnet',
+			// 	allowAdditionalConnection: false,
+			// },
 		] as RpcEndpoint[],
 		mainnet: [
 			{
