7074355534d3a49e585042c318ee2906fd05ab6b
Luke Steyn <lukecaan@gmail.com>
2023-11-01 15:18:20 +0100

    Updated icons to avoid annoying console errors

--- icons/src/icons/components/Fullsize.tsx
+++ icons/src/icons/components/Fullsize.tsx
@@ -8,8 +8,8 @@ const Fullsize = (allProps: IconProps) => {
 		<IconWrapper
 			icon={
 				<svg
-					width="inherit"
-					height="inherit"
+					width="100%"
+					height="100%"
 					viewBox="0 0 16 16"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
