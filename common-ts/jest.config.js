5d290b07b92f90cdc99f4164b3f511432acd196a
Chester Sim <chestersimdy@gmail.com>
2023-11-16 17:34:43 +0800

    ci(common-ts): remove old test config

--- common-ts/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-	preset: 'ts-jest/presets/default-esm',
-	testEnvironment: 'node',
-	moduleFileExtensions: ['ts', 'js'],
-	testRegex: '.test.ts',
-	transform: {},
-};
