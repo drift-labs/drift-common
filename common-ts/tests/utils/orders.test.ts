48a355d7c617253859293f38ea62081f9bc2bd28
lowkeynicc <nick@lunoho.company>
2023-12-16 14:36:08 -0500

    added more tests

--- common-ts/tests/utils/orders.test.ts
+++ common-ts/tests/utils/orders.test.ts
@@ -1,5 +1,14 @@
-import { BN, PRICE_PRECISION, PositionDirection } from '@drift-labs/sdk';
-import { COMMON_UI_UTILS } from '@drift/common';
+import {
+	BASE_PRECISION,
+	BN,
+	BigNum,
+	MarketType,
+	OrderType,
+	PRICE_PRECISION,
+	PRICE_PRECISION_EXP,
+	PositionDirection,
+} from '@drift-labs/sdk';
+import { COMMON_UI_UTILS, ENUM_UTILS } from '@drift/common';
 import { expect } from 'chai';
 
 describe('COMMON_UI_UTILS OrderParams Tests', () => {
@@ -12,14 +21,184 @@ describe('COMMON_UI_UTILS OrderParams Tests', () => {
 				limitPrice: new BN(103).mul(PRICE_PRECISION),
 				duration: 20,
 				marketTickSize: new BN(1),
-				auctionStartPriceOffset: 0.05,
+				auctionStartPriceOffset: -0.05,
 				auctionEndPriceOffset: 0.05,
 			});
-			expect(result.auctionDuration).to.equal(20);
+
 			expect(result.auctionStartPrice.toString()).to.equal('99950000');
 			expect(result.auctionEndPrice.toString()).to.equal('103000000');
 		});
 
-		// todo - more tests
+		it('should correctly generate params for short', () => {
+			const result = COMMON_UI_UTILS.getMarketAuctionParams({
+				direction: PositionDirection.SHORT,
+				startPriceFromSettings: new BN(100).mul(PRICE_PRECISION),
+				worstPrice: new BN(95).mul(PRICE_PRECISION),
+				limitPrice: new BN(97).mul(PRICE_PRECISION),
+				duration: 20,
+				marketTickSize: new BN(1),
+				auctionStartPriceOffset: -0.05,
+				auctionEndPriceOffset: 0.05,
+			});
+
+			expect(result.auctionStartPrice.toString()).to.equal('100050000');
+			expect(result.auctionEndPrice.toString()).to.equal('97000000');
+		});
+	});
+
+	describe('getLimitAuctionParams', () => {
+		it('should correctly generate params for long', () => {
+			const result = COMMON_UI_UTILS.getLimitAuctionParams({
+				direction: PositionDirection.LONG,
+				inputPrice: BigNum.from(
+					new BN(108).mul(PRICE_PRECISION),
+					PRICE_PRECISION_EXP
+				),
+				startPriceFromSettings: new BN(100).mul(PRICE_PRECISION),
+				duration: 60,
+				auctionStartPriceOffset: -0.05,
+			});
+
+			expect(result.auctionStartPrice.toString()).to.equal('99946000');
+			expect(result.auctionEndPrice.toString()).to.equal('108000000');
+		});
+
+		it('should correctly generate params for short', () => {
+			const result = COMMON_UI_UTILS.getLimitAuctionParams({
+				direction: PositionDirection.SHORT,
+				inputPrice: BigNum.from(
+					new BN(92).mul(PRICE_PRECISION),
+					PRICE_PRECISION_EXP
+				),
+				startPriceFromSettings: new BN(100).mul(PRICE_PRECISION),
+				duration: 60,
+				auctionStartPriceOffset: -0.05,
+			});
+
+			expect(result.auctionStartPrice.toString()).to.equal('100046000');
+			expect(result.auctionEndPrice.toString()).to.equal('92000000');
+		});
+	});
+
+	describe('deriveMarketOrderParams', () => {
+		it('should correctly generate params for LONG oracle order when oracle > est entry', () => {
+			const result = COMMON_UI_UTILS.deriveMarketOrderParams({
+				marketType: MarketType.PERP,
+				marketIndex: 0,
+				direction: PositionDirection.LONG,
+				maxLeverageSelected: false,
+				maxLeverageOrderSize: new BN(1000).mul(BASE_PRECISION),
+				baseAmount: new BN(1).mul(BASE_PRECISION),
+				reduceOnly: false,
+				allowInfSlippage: false,
+				bestPrice: new BN(99).mul(PRICE_PRECISION),
+				entryPrice: new BN(100).mul(PRICE_PRECISION),
+				oraclePrice: new BN(108).mul(PRICE_PRECISION),
+				worstPrice: new BN(105).mul(PRICE_PRECISION),
+				limitPrice: new BN(103).mul(PRICE_PRECISION),
+				auctionDuration: 20,
+				marketTickSize: new BN(1),
+				auctionStartPriceOffset: -0.05,
+				auctionEndPriceOffset: 0.05,
+				auctionStartPriceOffsetFrom: 'entry',
+				isOracleOrder: true,
+			});
+
+			expect(ENUM_UTILS.toStr(result.orderType)).to.equal(
+				ENUM_UTILS.toStr(OrderType.ORACLE)
+			);
+			expect(result.auctionStartPrice?.toString()).to.equal('-8050000');
+			expect(result.auctionEndPrice?.toString()).to.equal('-5000000');
+		});
+
+		it('should correctly generate params for LONG oracle order when oracle < est entry', () => {
+			const result = COMMON_UI_UTILS.deriveMarketOrderParams({
+				marketType: MarketType.PERP,
+				marketIndex: 0,
+				direction: PositionDirection.LONG,
+				maxLeverageSelected: false,
+				maxLeverageOrderSize: new BN(1000).mul(BASE_PRECISION),
+				baseAmount: new BN(1).mul(BASE_PRECISION),
+				reduceOnly: false,
+				allowInfSlippage: false,
+				bestPrice: new BN(99).mul(PRICE_PRECISION),
+				entryPrice: new BN(100).mul(PRICE_PRECISION),
+				oraclePrice: new BN(92).mul(PRICE_PRECISION),
+				worstPrice: new BN(105).mul(PRICE_PRECISION),
+				limitPrice: new BN(103).mul(PRICE_PRECISION),
+				auctionDuration: 20,
+				marketTickSize: new BN(1),
+				auctionStartPriceOffset: -0.05,
+				auctionEndPriceOffset: 0.05,
+				auctionStartPriceOffsetFrom: 'entry',
+				isOracleOrder: true,
+			});
+
+			expect(ENUM_UTILS.toStr(result.orderType)).to.equal(
+				ENUM_UTILS.toStr(OrderType.ORACLE)
+			);
+			expect(result.auctionStartPrice?.toString()).to.equal('7950000');
+			expect(result.auctionEndPrice?.toString()).to.equal('11000000');
+		});
+
+		it('should correctly generate params for SHORT oracle order when oracle < est entry', () => {
+			const result = COMMON_UI_UTILS.deriveMarketOrderParams({
+				marketType: MarketType.PERP,
+				marketIndex: 0,
+				direction: PositionDirection.SHORT,
+				maxLeverageSelected: false,
+				maxLeverageOrderSize: new BN(1000).mul(BASE_PRECISION),
+				baseAmount: new BN(1).mul(BASE_PRECISION),
+				reduceOnly: false,
+				allowInfSlippage: false,
+				bestPrice: new BN(101).mul(PRICE_PRECISION),
+				entryPrice: new BN(100).mul(PRICE_PRECISION),
+				oraclePrice: new BN(92).mul(PRICE_PRECISION),
+				worstPrice: new BN(95).mul(PRICE_PRECISION),
+				limitPrice: new BN(98).mul(PRICE_PRECISION),
+				auctionDuration: 20,
+				marketTickSize: new BN(1),
+				auctionStartPriceOffset: -0.05,
+				auctionEndPriceOffset: 0.05,
+				auctionStartPriceOffsetFrom: 'entry',
+				isOracleOrder: true,
+			});
+
+			expect(ENUM_UTILS.toStr(result.orderType)).to.equal(
+				ENUM_UTILS.toStr(OrderType.ORACLE)
+			);
+			expect(result.auctionStartPrice?.toString()).to.equal('8050000');
+			expect(result.auctionEndPrice?.toString()).to.equal('6000000');
+		});
+
+		it('should correctly generate params for SHORT oracle order when oracle > est entry', () => {
+			const result = COMMON_UI_UTILS.deriveMarketOrderParams({
+				marketType: MarketType.PERP,
+				marketIndex: 0,
+				direction: PositionDirection.SHORT,
+				maxLeverageSelected: false,
+				maxLeverageOrderSize: new BN(1000).mul(BASE_PRECISION),
+				baseAmount: new BN(1).mul(BASE_PRECISION),
+				reduceOnly: false,
+				allowInfSlippage: false,
+				bestPrice: new BN(101).mul(PRICE_PRECISION),
+				entryPrice: new BN(100).mul(PRICE_PRECISION),
+				oraclePrice: new BN(108).mul(PRICE_PRECISION),
+				worstPrice: new BN(95).mul(PRICE_PRECISION),
+				limitPrice: new BN(98).mul(PRICE_PRECISION),
+				auctionDuration: 20,
+				marketTickSize: new BN(1),
+				auctionStartPriceOffset: -0.05,
+				auctionEndPriceOffset: 0.05,
+				auctionStartPriceOffsetFrom: 'entry',
+				isOracleOrder: true,
+			});
+
+			expect(ENUM_UTILS.toStr(result.orderType)).to.equal(
+				ENUM_UTILS.toStr(OrderType.ORACLE)
+			);
+			expect(result.auctionStartPrice?.toString()).to.equal('-7950000');
+			expect(result.auctionEndPrice?.toString()).to.equal('-10000000');
+		});
 	});
 });
