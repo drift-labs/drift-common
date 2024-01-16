cf153aececdf66bf05a45858dbddc78481962fca
Luke Steyn <lukecaan@gmail.com>
2024-01-16 23:24:17 +0300

    Prettier bump

--- common-ts/src/utils/Stopwatch.ts
+++ common-ts/src/utils/Stopwatch.ts
@@ -1,29 +1,29 @@
-
-
 /**
  * A Dev Tool to help with performance analysis.
- * 
+ *
  * The stopwatch class should function as a "timer" for a variety of unique keys. Any time a caller calls the "tick" method, with a key, the stopwatch will log the time since the last tick for that key.
  */
 export class Stopwatch {
-    private _lastTick: { [key: string]: number } = {};
-    constructor() { }
+	private _lastTick: { [key: string]: number } = {};
+	constructor() {}
 
-    /**
-     * Logs the time since the last tick for the given key.
-     * @param key The key to log the time for.
-     */
-    public tick(key: string, message?:string) {
-        const now = Date.now();
-        const lastTick = this._lastTick[key];
-        if (lastTick) {
-            console.log(`⏱️ :: ${message ?` ${message} `:''} \n${key} => ${now - lastTick}ms`);
-        }
-        this._lastTick[key] = now;
-    }
+	/**
+	 * Logs the time since the last tick for the given key.
+	 * @param key The key to log the time for.
+	 */
+	public tick(key: string, message?: string) {
+		const now = Date.now();
+		const lastTick = this._lastTick[key];
+		if (lastTick) {
+			console.log(
+				`⏱️ :: ${message ? ` ${message} ` : ''} \n${key} => ${now - lastTick}ms`
+			);
+		}
+		this._lastTick[key] = now;
+	}
 
-    blankTick(key: string) {
-        const now = Date.now();
-        this._lastTick[key] = now;
-    }
+	blankTick(key: string) {
+		const now = Date.now();
+		this._lastTick[key] = now;
+	}
 }
