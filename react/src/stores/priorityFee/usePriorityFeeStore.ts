9291d59bc1fa693579a4c5a044188a64c20e30f2
Chester Sim <chestersimdy@gmail.com>
2024-03-15 16:21:22 +0800

    refactor(react): add docs

--- react/src/stores/priorityFee/usePriorityFeeStore.ts
+++ react/src/stores/priorityFee/usePriorityFeeStore.ts
@@ -17,6 +17,12 @@ export type PriorityFeeStore = {
 	};
 };
 
+/**
+ * To use this store, you need to first import and call `useSyncPriorityFeeStore` at the top of your components.
+ * You may use the hook `usePriorityFeeUserSettings` to fetch and update the user's priority fee settings in local storage.
+ * The compute units price to use can be obtained through this store's `getPriorityFeeToUse` function, and should be used
+ * to supplement the transaction params when building and sending a transaction.
+ */
 export const usePriorityFeeStore = create(
 	devtools<PriorityFeeStore>((set, get) => ({
 		set: (fn) => set(produce(fn)),
