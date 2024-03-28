3af8a5a76939ef5a968cc42e66823fa75406c11f
Chester Sim <chestersimdy@gmail.com>
2024-03-28 13:58:25 +0800

    refactor(icons): update icons

--- icons/src/icons/components/Trade.tsx
+++ icons/src/icons/components/Trade.tsx
@@ -16,19 +16,7 @@ const Trade = (allProps: IconProps) => {
 					<path
 						fillRule="evenodd"
 						clipRule="evenodd"
-						d="M4.5 6.5v10h5v-10h-5zM4 5a1 1 0 00-1 1v11a1 1 0 001 1h6a1 1 0 001-1V6a1 1 0 00-1-1H4z"
-						fill={allProps.color ? allProps.color : 'currentColor'}
-					/>
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="M7 2.25a.75.75 0 01.75.75v2a.75.75 0 01-1.5 0V3A.75.75 0 017 2.25zM7 17.25a.75.75 0 01.75.75v3a.75.75 0 01-1.5 0v-3a.75.75 0 01.75-.75zM14.5 10.5v4h5v-4h-5zM14 9a1 1 0 00-1 1v5a1 1 0 001 1h6a1 1 0 001-1v-5a1 1 0 00-1-1h-6z"
-						fill={allProps.color ? allProps.color : 'currentColor'}
-					/>
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="M17 2.25a.75.75 0 01.75.75v6a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM17 14.75a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-5.5a.75.75 0 01.75-.75z"
+						d="M7.75 3a.75.75 0 00-1.5 0v2H4a1 1 0 00-1 1v11a1 1 0 001 1h2.25v3a.75.75 0 001.5 0v-3H10a1 1 0 001-1V6a1 1 0 00-1-1H7.75V3zM4.5 16.5v-10h5v10h-5zm10-2v-4h5v4h-5zM13 10a1 1 0 011-1h2.25V3a.75.75 0 011.5 0v6H20a1 1 0 011 1v5a1 1 0 01-1 1h-2.25v5a.75.75 0 01-1.5 0v-5H14a1 1 0 01-1-1v-5z"
 						fill={allProps.color ? allProps.color : 'currentColor'}
 					/>
 				</svg>
