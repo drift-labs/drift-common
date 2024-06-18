8b4d53190c437f573666cc55d416fd42bd0b1adb
Luke Steyn <lukecaan@gmail.com>
2024-06-18 17:10:01 +0800

    Bump improved enum util test

--- common-ts/tests/utils/enumUtils.test.ts
+++ common-ts/tests/utils/enumUtils.test.ts
@@ -1,18 +1,79 @@
-import { OrderActionExplanation } from '@drift-labs/sdk';
+import {
+	AssetTier,
+	ContractTier,
+	ContractType,
+	DepositDirection,
+	DepositExplanation,
+	LPAction,
+	LiquidationType,
+	MarketStatus,
+	MarketType,
+	ModifyOrderPolicy,
+	OracleSource,
+	OrderAction,
+	OrderActionExplanation,
+	OrderStatus,
+	OrderTriggerCondition,
+	OrderType,
+	PositionDirection,
+	PostOnlyParams,
+	SettlePnlExplanation,
+	SettlePnlMode,
+	SpotBalanceType,
+	SpotFulfillmentConfigStatus,
+	SpotFulfillmentStatus,
+	SpotFulfillmentType,
+	StakeAction,
+	SwapDirection,
+	SwapReduceOnly,
+} from '@drift-labs/sdk';
 import { expect } from 'chai';
 import { ENUM_UTILS } from '../../src/utils';
 
 describe('Enum Utils', () => {
 	describe('ENUM_UTILS are consistent', () => {
+		const ENUMS_TO_CHECK = [
+			MarketStatus,
+			ContractType,
+			ContractTier,
+			AssetTier,
+			SwapDirection,
+			SpotBalanceType,
+			PositionDirection,
+			DepositDirection,
+			OracleSource,
+			OrderType,
+			MarketType,
+			OrderStatus,
+			OrderAction,
+			OrderActionExplanation,
+			OrderTriggerCondition,
+			SpotFulfillmentType,
+			SpotFulfillmentStatus,
+			DepositExplanation,
+			SettlePnlExplanation,
+			SpotFulfillmentConfigStatus,
+			StakeAction,
+			SettlePnlMode,
+			LPAction,
+			LiquidationType,
+			PostOnlyParams,
+			ModifyOrderPolicy,
+			SwapReduceOnly,
+		];
+
 		it('should be consistent when converting between strings and objects', () => {
-			const actionExplanation =
-				OrderActionExplanation.ORDER_FILLED_WITH_AMM_JIT;
-			const stringedActionExplanation = ENUM_UTILS.toStr(actionExplanation);
-			const backToObjActionExplanation = ENUM_UTILS.toObj(
-				stringedActionExplanation
-			);
-			expect(ENUM_UTILS.match(actionExplanation, backToObjActionExplanation)).to
-				.be.true;
+			for (const ENUM of ENUMS_TO_CHECK) {
+				const enumEntries = Object.entries(ENUM);
+
+				for (const [_key, value] of enumEntries) {
+					const stringedValue = ENUM_UTILS.toStr(value);
+					const backToObjValue = ENUM_UTILS.toObj(stringedValue);
+
+					expect(ENUM_UTILS.toStr(value) === stringedValue).to.be.true;
+					expect(ENUM_UTILS.match(value, backToObjValue)).to.be.true;
+				}
+			}
 		});
 	});
 });
