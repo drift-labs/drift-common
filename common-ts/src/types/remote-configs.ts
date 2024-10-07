fd310d90c3dbce0c3c56cdaf5878a37b2a9faa1b
Chester Sim <chestersimdy@gmail.com>
2024-10-07 18:37:15 +0800

    refactor(common-ts): abstract serialized remote config types

---
+++ common-ts/src/types/remote-configs.ts
@@ -0,0 +1,60 @@
+import { PerpMarketConfig, SpotMarketConfig } from '@drift-labs/sdk';
+
+export interface IPredictionMarketConfig {
+	title: string;
+	shortTitle: string;
+	resolutionDateUnix: number;
+	category?: string;
+	symbol: string;
+	imgSrc: string;
+	previewImgSrc?: string;
+	devnetMarketIndex?: number;
+	mainnetMarketIndex?: number;
+	production?: boolean;
+	endingSoonThresholdDays?: number;
+}
+
+export type SerializedSpotMarketConfig = Omit<
+	SpotMarketConfig,
+	| 'oracle'
+	| 'mint'
+	| 'oracleSource'
+	| 'precision'
+	| 'precisionExp'
+	| 'serumMarket'
+	| 'phoenixMarket'
+	| 'openbookMarket'
+> & {
+	oracle: string;
+	mint: string;
+	oracleSource: string;
+	precision: number;
+	precisionExp: number;
+	serumMarket?: string;
+	phoenixMarket?: string;
+	openbookMarket?: string;
+	production?: boolean;
+};
+
+export type SerializedPerpMarketConfig = Omit<
+	PerpMarketConfig,
+	'oracle' | 'oracleSource'
+> & {
+	oracle: string;
+	oracleSource: string;
+	production?: boolean;
+};
+
+export type SerializedRemoteConfigs = {
+	marketConfigs: {
+		devnet: {
+			spot: SerializedSpotMarketConfig[];
+			perp: SerializedPerpMarketConfig[];
+		};
+		mainnet: {
+			spot: SerializedSpotMarketConfig[];
+			perp: SerializedPerpMarketConfig[];
+		};
+	};
+	predictionMarkets: IPredictionMarketConfig[];
+};
