3a41018dd04b5ac1ddf113b816921d81baff1023
Evan Pipta <3pipta@gmail.com>
2024-12-03 11:40:31 -0800

    common: change market lookups to objects

--- common-ts/src/Config.ts
+++ common-ts/src/Config.ts
@@ -8,32 +8,24 @@ import { JLP_POOL_ID, MAIN_POOL_ID } from './constants';
 
 export const Config: {
 	initialized: boolean;
-	spotMarketsLookup: SpotMarketConfig[];
-	jlpSpotMarketsLookup: SpotMarketConfig[];
-	perpMarketsLookup: PerpMarketConfig[];
+	spotMarketsLookup: Record<number, SpotMarketConfig>;
+	jlpSpotMarketsLookup: Record<number, SpotMarketConfig>;
+	perpMarketsLookup: Record<number, PerpMarketConfig>;
 	sdkConfig: ReturnType<typeof initialize>;
 } = {
 	initialized: false,
-	spotMarketsLookup: [],
-	jlpSpotMarketsLookup: [],
-	perpMarketsLookup: [],
+	spotMarketsLookup: {},
+	jlpSpotMarketsLookup: {},
+	perpMarketsLookup: {},
 	sdkConfig: undefined,
 };
 
 export const Initialize = (env: DriftEnv) => {
 	const SDKConfig = initialize({ env });
 
-	const maxSpotMarketIndex = Math.max(
-		...SDKConfig.SPOT_MARKETS.map((market) => market.marketIndex)
-	);
-
-	const maxPerpMarketIndex = Math.max(
-		...SDKConfig.PERP_MARKETS.map((market) => market.marketIndex)
-	);
-
-	const jlpSpotMarkets = new Array(maxSpotMarketIndex);
-	const spotMarkets = new Array(maxSpotMarketIndex);
-	const markets = new Array(maxPerpMarketIndex);
+	const jlpSpotMarkets = {};
+	const spotMarkets = {};
+	const markets = {};
 
 	SDKConfig.SPOT_MARKETS.filter(
 		(market) => market.poolId === MAIN_POOL_ID
