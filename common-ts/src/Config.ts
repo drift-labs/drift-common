671748bdca57867b240154bfb0ad72d7f9720699
Evan Pipta <3pipta@gmail.com>
2024-12-04 15:30:06 -0800

    common: revert jlp pool markets lookup

--- common-ts/src/Config.ts
+++ common-ts/src/Config.ts
@@ -4,18 +4,15 @@ import {
 	initialize,
 	PerpMarketConfig,
 } from '@drift-labs/sdk';
-import { JLP_POOL_ID, MAIN_POOL_ID } from './constants';
 
 export const Config: {
 	initialized: boolean;
 	spotMarketsLookup: SpotMarketConfig[];
-	jlpSpotMarketsLookup: SpotMarketConfig[];
 	perpMarketsLookup: PerpMarketConfig[];
 	sdkConfig: ReturnType<typeof initialize>;
 } = {
 	initialized: false,
 	spotMarketsLookup: [],
-	jlpSpotMarketsLookup: [],
 	perpMarketsLookup: [],
 	sdkConfig: undefined,
 };
@@ -31,20 +28,11 @@ export const Initialize = (env: DriftEnv) => {
 		...SDKConfig.PERP_MARKETS.map((market) => market.marketIndex)
 	);
 
-	const jlpSpotMarkets = new Array(maxSpotMarketIndex);
 	const spotMarkets = new Array(maxSpotMarketIndex);
 	const markets = new Array(maxPerpMarketIndex);
 
-	SDKConfig.SPOT_MARKETS.filter(
-		(market) => market.poolId === MAIN_POOL_ID
-	).forEach((spotMarket) => {
+	SDKConfig.SPOT_MARKETS.forEach((spotMarket) => {
 		spotMarkets[spotMarket.marketIndex] = spotMarket;
-	});
-
-	SDKConfig.SPOT_MARKETS.filter(
-		(market) => market.poolId === JLP_POOL_ID
-	).forEach((spotMarket) => {
-		jlpSpotMarkets[spotMarket.marketIndex] = spotMarket;
 	});
 
 	SDKConfig.PERP_MARKETS.forEach((perpMarket) => {
@@ -52,7 +40,6 @@ export const Initialize = (env: DriftEnv) => {
 	});
 
 	Config.spotMarketsLookup = spotMarkets;
-	Config.jlpSpotMarketsLookup = jlpSpotMarkets;
 	Config.perpMarketsLookup = markets;
 
 	Config.initialized = true;
