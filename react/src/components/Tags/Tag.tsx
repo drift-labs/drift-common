da5bafa70c1dc70e7d76f43f98c190aaffd92255
Lukas deConantsesznak <lukas@lunoho.company>
2025-02-05 10:28:36 -0700

    feat: warning tag color

--- react/src/components/Tags/Tag.tsx
+++ react/src/components/Tags/Tag.tsx
@@ -1,7 +1,13 @@
 import { Typo } from '../Text/Typo';
 import { twMerge } from 'tailwind-merge';
 
-export type TagColor = 'green' | 'purple' | 'yellow' | 'default' | 'red';
+export type TagColor =
+	| 'green'
+	| 'purple'
+	| 'yellow'
+	| 'warning'
+	| 'default'
+	| 'red';
 
 export type TagProps = {
 	children: React.ReactNode;
@@ -18,6 +24,7 @@ const TAG_COLORS: Record<TagColor, { text: string; bg: string }> = {
 		bg: 'bg-interactive-secondary-bg',
 	},
 	yellow: { text: 'text-yellow-50', bg: 'bg-yellow-50/20' },
+	warning: { text: 'text-warn-yellow', bg: 'bg-brand-yellow-secondary-bg' },
 	default: { text: 'text-default', bg: 'bg-button-secondary-bg' },
 	red: {
 		text: 'text-text-negative-red-button',
