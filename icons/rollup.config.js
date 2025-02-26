f7d58f47d582e9cd1b5bd0aff84fbfa69cf86448
Lukas deConantsesznak <lukas@lunoho.company>
2025-02-26 15:12:31 -0700

    fix: no cjs on icons output

--- icons/rollup.config.js
+++ icons/rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
@@ -9,11 +8,11 @@ export default [
 	{
 		input: 'src/index.ts',
 		output: [
-			{
-				file: packageJson.main,
-				format: 'cjs',
-				sourcemap: true,
-			},
+			// {
+			// 	file: packageJson.main,
+			// 	format: 'cjs',
+			// 	sourcemap: true,
+			// },
 			{
 				file: packageJson.module,
 				format: 'esm',
@@ -22,7 +21,7 @@ export default [
 		],
 		plugins: [
 			resolve(),
-			commonjs(),
+			// commonjs(),
 			typescript({ tsconfig: './tsconfig.json' }),
 		],
 	},
