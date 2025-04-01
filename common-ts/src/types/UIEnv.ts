129739ceed9e5a1bca35a4b9d4bf4e35e527b5d4
Luke Steyn <lukecaan@gmail.com>
2025-04-01 17:34:51 +0900

    Added UIEnv utility class. Updated candle client to handle staging environments in the UI

---
+++ common-ts/src/types/UIEnv.ts
@@ -0,0 +1,58 @@
+import { DriftEnv } from '@drift-labs/sdk';
+import { Opaque } from '.';
+
+// Define UIEnvKey as an opaque type
+export type UIEnvKey = Opaque<'UIEnvKey', string>;
+
+/**
+ * Utility class to handle environment configs in the Drift UI. Note that these environments don't exactly align with the SDK environment (SDK can only be mainnet or devnet, but UI can be devnet, staging, mainnet) so there is a `sdkEnv` getter utility to convert the UI env to SDK env (UI staging => SDK mainnet).
+ */
+export class UIEnv {
+	constructor(readonly env: DriftEnv | 'staging') {}
+
+	static createMainnet() {
+		return new UIEnv('mainnet' as DriftEnv);
+	}
+
+	static createDevnet() {
+		return new UIEnv('devnet' as DriftEnv);
+	}
+
+	static createStaging() {
+		return new UIEnv('staging');
+	}
+
+	static getUIEnvIdFromKey(key: UIEnvKey) {
+		return new UIEnv(key as DriftEnv | 'staging');
+	}
+
+	get isMainnet() {
+		return this.env === ('mainnet' as DriftEnv);
+	}
+
+	get isDevnet() {
+		return this.env === ('devnet' as DriftEnv);
+	}
+
+	get isStaging() {
+		return this.env === 'staging';
+	}
+
+	/**
+	 * Returns the SDK environment type. Note that 'staging' maps to 'mainnet' for SDK purposes.
+	 */
+	get sdkEnv(): DriftEnv {
+		return this.isStaging ? ('mainnet' as DriftEnv) : (this.env as DriftEnv);
+	}
+
+	/**
+	 * Returns a unique string that can be used as a key in a map.
+	 */
+	get key() {
+		return this.env as UIEnvKey;
+	}
+
+	equals(other: UIEnv) {
+		return this.env === other.env;
+	}
+}
