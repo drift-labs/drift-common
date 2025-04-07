6575a71b6ab3b6a29e945edc33855cc117707047
Lukas deConantsesznak <lukas@lunoho.company>
2025-04-07 11:54:11 -0600

    fix: CircularBuffer tests used bun and needed chai API for CI

--- common-ts/tests/utils/CircularBuffer.test.ts
+++ common-ts/tests/utils/CircularBuffer.test.ts
@@ -1,4 +1,5 @@
 import { CircularBuffer } from '../../src/utils/CircularBuffers';
+import { expect } from 'chai';
 
 describe('CircularBuffer', () => {
 	let buffer: CircularBuffer<number>;
@@ -7,60 +8,60 @@ describe('CircularBuffer', () => {
 		buffer = new CircularBuffer<number>(3);
 	});
 
-	test('should_create_an_empty_buffer', () => {
-		expect(buffer.toArray()).toEqual([]);
+	it('should_create_an_empty_buffer', () => {
+		expect(buffer.toArray()).to.deep.equal([]);
 	});
 
-	test('should_add_elements_to_the_buffer', () => {
+	it('should_add_elements_to_the_buffer', () => {
 		buffer.add(1);
 		buffer.add(2);
-		expect(buffer.toArray()).toEqual([1, 2]);
+		expect(buffer.toArray()).to.deep.equal([1, 2]);
 	});
 
-	test('should_maintain_circular_structure', () => {
-		buffer.add(1);
-		buffer.add(2);
-		buffer.add(3);
-		expect(buffer.toArray()).toEqual([1, 2, 3]);
-		expect(buffer.add(4)).toBe(1);
-		expect(buffer.toArray()).toEqual([2, 3, 4]);
-	});
-
-	test('should_return_null_when_buffer_is_not_full', () => {
-		expect(buffer.add(1)).toBeNull();
-		expect(buffer.add(2)).toBeNull();
-		expect(buffer.toArray()).toEqual([1, 2]);
-	});
-
-	test('should_return_the_removed_element_when_buffer_is_full', () => {
+	it('should_maintain_circular_structure', () => {
 		buffer.add(1);
 		buffer.add(2);
 		buffer.add(3);
-		expect(buffer.add(4)).toBe(1);
-		expect(buffer.toArray()).toEqual([2, 3, 4]);
+		expect(buffer.toArray()).to.deep.equal([1, 2, 3]);
+		expect(buffer.add(4)).to.equal(1);
+		expect(buffer.toArray()).to.deep.equal([2, 3, 4]);
 	});
 
-	test('should_handle_adding_elements_to_a_full_buffer', () => {
+	it('should_return_null_when_buffer_is_not_full', () => {
+		expect(buffer.add(1)).to.be.null;
+		expect(buffer.add(2)).to.be.null;
+		expect(buffer.toArray()).to.deep.equal([1, 2]);
+	});
+
+	it('should_return_the_removed_element_when_buffer_is_full', () => {
+		buffer.add(1);
+		buffer.add(2);
+		buffer.add(3);
+		expect(buffer.add(4)).to.equal(1);
+		expect(buffer.toArray()).to.deep.equal([2, 3, 4]);
+	});
+
+	it('should_handle_adding_elements_to_a_full_buffer', () => {
 		buffer.add(1);
 		buffer.add(2);
 		buffer.add(3);
 		buffer.add(4);
 		buffer.add(5);
-		expect(buffer.toArray()).toEqual([3, 4, 5]);
+		expect(buffer.toArray()).to.deep.equal([3, 4, 5]);
 	});
 
-	test('should_handle_a_buffer_with_capacity_1', () => {
+	it('should_handle_a_buffer_with_capacity_1', () => {
 		const singleBuffer = new CircularBuffer<number>(1);
-		expect(singleBuffer.add(1)).toBeNull();
-		expect(singleBuffer.add(2)).toBe(1);
-		expect(singleBuffer.toArray()).toEqual([2]);
+		expect(singleBuffer.add(1)).to.be.null;
+		expect(singleBuffer.add(2)).to.equal(1);
+		expect(singleBuffer.toArray()).to.deep.equal([2]);
 	});
 
-	test('should_handle_adding_different_data_types', () => {
+	it('should_handle_adding_different_data_types', () => {
 		const stringBuffer = new CircularBuffer<string>(2);
 		stringBuffer.add('a');
 		stringBuffer.add('b');
-		expect(stringBuffer.add('c')).toBe('a');
-		expect(stringBuffer.toArray()).toEqual(['b', 'c']);
+		expect(stringBuffer.add('c')).to.equal('a');
+		expect(stringBuffer.toArray()).to.deep.equal(['b', 'c']);
 	});
 });
