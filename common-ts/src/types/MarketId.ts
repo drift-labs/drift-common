cbc673a62a00dd66d1ccf980b9f74832da462eed
Luke <lukecaan@gmail.com>
2025-06-30 17:54:01 +0800

    Implemented performance optimisations on MarketId and UIMarket classes (#206)

--- common-ts/src/types/MarketId.ts
+++ common-ts/src/types/MarketId.ts
@@ -6,40 +6,71 @@ import { Opaque } from '.';
 export type MarketKey = Opaque<'MarketKey', string>;
 
 export class MarketId {
-	constructor(readonly marketIndex: number, readonly marketType: MarketType) {}
+	private static cache = new Map<MarketKey, MarketId>();
+	private _key: MarketKey;
+	private _isSpot: boolean;
+	private _isPerp: boolean;
+	private _marketTypeStr: string;
+
+	constructor(readonly marketIndex: number, readonly marketType: MarketType) {
+		this._key = MarketId.key(marketIndex, marketType);
+		this._isSpot = ENUM_UTILS.match(marketType, MarketType.SPOT);
+		this._isPerp = ENUM_UTILS.match(marketType, MarketType.PERP);
+		this._marketTypeStr = ENUM_UTILS.toStr(marketType);
+	}
+
+	private static getOrCreate(marketIndex: number, marketType: MarketType) {
+		const key = MarketId.key(marketIndex, marketType);
+		if (MarketId.cache.has(key)) {
+			return MarketId.cache.get(key)!;
+		}
+		const marketId = new MarketId(marketIndex, marketType);
+		MarketId.cache.set(key, marketId);
+		return marketId;
+	}
 
 	static createPerpMarket(marketIndex: number) {
-		return new MarketId(marketIndex, MarketType.PERP);
+		return MarketId.getOrCreate(marketIndex, MarketType.PERP);
 	}
 
 	static createSpotMarket(marketIndex: number) {
-		return new MarketId(marketIndex, MarketType.SPOT);
+		return MarketId.getOrCreate(marketIndex, MarketType.SPOT);
 	}
 
 	static getMarketIdFromKey(key: MarketKey) {
+		if (MarketId.cache.has(key)) {
+			return MarketId.cache.get(key)!;
+		}
 		const [marketType, marketIndex] = key.split('_');
-		return new MarketId(parseInt(marketIndex), ENUM_UTILS.toObj(marketType));
+		const marketId = new MarketId(
+			parseInt(marketIndex),
+			ENUM_UTILS.toObj(marketType)
+		);
+		MarketId.cache.set(key, marketId);
+		return marketId;
 	}
 
 	get isSpot() {
-		return ENUM_UTILS.match(this.marketType, MarketType.SPOT);
+		return this._isSpot;
 	}
 
 	get isPerp() {
-		return ENUM_UTILS.match(this.marketType, MarketType.PERP);
+		return this._isPerp;
 	}
 
 	get marketTypeStr() {
-		return ENUM_UTILS.toStr(this.marketType);
+		return this._marketTypeStr;
+	}
+
+	static key(marketIndex: number, marketType: MarketType): MarketKey {
+		return `${ENUM_UTILS.toStr(marketType)}_${marketIndex}` as MarketKey;
 	}
 
 	/**
 	 * Returns a unique string that can be used as a key in a map.
 	 */
-	get key() {
-		return `${ENUM_UTILS.toStr(this.marketType)}_${
-			this.marketIndex
-		}` as MarketKey;
+	get key(): MarketKey {
+		return this._key; // Micro-optimisation to avoid string concatenation every time the key is accessed
 	}
 
 	equals(other: MarketId) {
