aa1eb0f04811244673de07f0626efb9819461461
Nick Caradonna <lowkeynicc@gmail.com>
2025-08-21 18:28:42 -0400

    implement market open interest and funding

--- common-ts/src/drift/base/details/market/openInterest.ts
+++ common-ts/src/drift/base/details/market/openInterest.ts
@@ -1,3 +1,17 @@
-export const getMarketOpenInterest = async () => {
-	// TODO:
+import { BASE_PRECISION_EXP, BigNum, DriftClient } from '@drift-labs/sdk';
+
+export const getMarketOpenInterest = (
+	driftClient: DriftClient,
+	marketIndex: number
+): { longOpenInterest: BigNum; shortOpenInterest: BigNum } => {
+	const perpMarketAccount = driftClient.getPerpMarketAccount(marketIndex);
+	const longOpenInterest = BigNum.from(
+		perpMarketAccount.amm.baseAssetAmountLong,
+		BASE_PRECISION_EXP
+	);
+	const shortOpenInterest = BigNum.from(
+		perpMarketAccount.amm.baseAssetAmountShort,
+		BASE_PRECISION_EXP
+	);
+	return { longOpenInterest, shortOpenInterest };
 };
