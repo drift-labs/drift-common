0c5a771f8b17e476b4a9c9588416cb39e8248d44
Lukas deConantsesznak <lukas@lunoho.company>
2025-11-06 15:49:56 -0700

    feat: more spot market index consts

--- common-ts/src/constants/markets.ts
+++ common-ts/src/constants/markets.ts
@@ -1,3 +1,7 @@
 export const USDC_SPOT_MARKET_INDEX = 0;
+export const SOL_SPOT_MARKET_INDEX = 1;
+export const WBTC_SPOT_MARKET_INDEX = 3;
+export const WETH_SPOT_MARKET_INDEX = 4;
+export const CBBTC_SPOT_MARKET_INDEX = 27;
 
 export const DEFAULT_MAX_MARKET_LEVERAGE = 10;
