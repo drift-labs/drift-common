b04de7d4fc1ca45ce4e48a8875667824e08ca6dc
LukasDeco <ldeconantsesznak@gmail.com>
2026-01-07 10:24:28 -0700

    feat: minified libs out here

---
+++ icons/build-browser.js
@@ -0,0 +1,73 @@
+const esbuild = require('esbuild');
+const path = require('path');
+const fs = require('fs');
+
+function getPackageExternalModules() {
+	const packageJsonPath = path.resolve(__dirname, 'package.json');
+	const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+	const dependencyNames = Object.keys(packageJson.dependencies ?? {});
+	const peerDependencyNames = Object.keys(packageJson.peerDependencies ?? {});
+
+	return Array.from(new Set([...dependencyNames, ...peerDependencyNames]));
+}
+
+async function build() {
+	try {
+		fs.mkdirSync(path.resolve(__dirname, 'dist'), { recursive: true });
+
+		await esbuild.build({
+			entryPoints: ['./dist/index.js'],
+			bundle: true,
+			minify: true,
+			format: 'cjs',
+			platform: 'browser',
+			target: ['es2020'],
+			outfile: './dist/browser.min.js',
+			external: getPackageExternalModules(),
+			define: {
+				'process.env.NODE_ENV': '"production"',
+				global: 'globalThis',
+			},
+			inject: [path.resolve(__dirname, 'esbuild-shims.js')],
+			alias: {
+				crypto: 'crypto-browserify',
+				stream: 'stream-browserify',
+				path: 'path-browserify',
+				os: 'os-browserify/browser',
+				process: 'process/browser',
+				vm: 'vm-browserify',
+			},
+			plugins: [
+				{
+					name: 'ignore-node-modules',
+					setup(build) {
+						build.onResolve(
+							{ filter: /^(fs|net|dns|tls|http2|child_process)$/ },
+							(args) => {
+								return { path: args.path, namespace: 'ignore-ns' };
+							}
+						);
+						build.onLoad({ filter: /.*/, namespace: 'ignore-ns' }, () => {
+							return { contents: 'export default {}' };
+						});
+					},
+				},
+			],
+			loader: {
+				'.json': 'json',
+			},
+			treeShaking: true,
+			sourcemap: false,
+		});
+
+		console.log('✅ @drift-labs/icons browser minify complete');
+	} catch (error) {
+		console.error('❌ Browser minify failed:', error);
+		process.exit(1);
+	}
+}
+
+build();
+
+
