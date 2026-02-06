2a1eb34db246ac93f2c122c9b9bb28800596952a
tmns <35785003+tmns@users.noreply.github.com>
2026-02-06 12:57:09 +0800

    Adjust icon colors (#311)

--- icons/src/icons/components/Warning.tsx
+++ icons/src/icons/components/Warning.tsx
@@ -15,7 +15,7 @@ const Warning = (allProps: IconProps) => {
 				>
 					<path
 						d="M19 3a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h14zm-7.5 13a1 1 0 00-1 1v.5a1 1 0 001 1h1a1 1 0 001-1V17a1 1 0 00-1-1h-1zm-.357-11a1 1 0 00-.992 1.132l1.095 8.207a.761.761 0 001.509 0l1.095-8.207A1 1 0 0012.858 5h-1.715z"
-						fill="#F2C94C"
+						fill={allProps.color ? allProps.color : 'currentColor'}
 					/>
 				</svg>
 			}
