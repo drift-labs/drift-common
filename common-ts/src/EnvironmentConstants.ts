a4de0c26f07f8b46f3141ab392e1cdd4a97ae6bf
Chester Sim <chestersimdy@gmail.com>
2026-06-30 12:14:41 +0800

    refactor(common-ts): update triton rpc endpoints

--- common-ts/src/EnvironmentConstants.ts
+++ common-ts/src/EnvironmentConstants.ts
@@ -16,18 +16,16 @@ export const EnvironmentConstants = {
 			},
 			{
 				label: 'RPC Pool',
-				value:
-					'https://drift-drift-a827.devnet.rpcpool.com/3639271b-6f0e-47c6-a643-1aaa0e498f58',
-				wsValue:
-					'wss://drift-drift-a827.devnet.rpcpool.com/3639271b-6f0e-47c6-a643-1aaa0e498f58/whirligig',
+				value: 'https://velocity-solanad-b50f.devnet.rpcpool.com',
+				wsValue: 'wss://velocity-solanad-b50f.devnet.rpcpool.com/whirligig',
 				allowAdditionalConnection: false,
 			},
 		] as RpcEndpoint[],
 		mainnet: [
 			{
 				label: 'Triton RPC Pool 1',
-				value: 'https://drift-drift_ma-39b5.mainnet.rpcpool.com/',
-				wsValue: 'wss://drift-drift_ma-39b5.mainnet.rpcpool.com/whirligig',
+				value: 'https://velocity-solanam-c448.mainnet.rpcpool.com',
+				wsValue: 'wss://velocity-solanam-c448.mainnet.rpcpool.com/whirligig',
 				allowAdditionalConnection: true,
 			},
 			{
